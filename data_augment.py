c5cf0208c48a919c1ef6fa09b26599d1eb2a7efc
Dias <37943216+DiasAzhigulov@users.noreply.github.com>
2020-04-25 00:50:17 +0600

    Update data_augment.py

--- data_augment.py
+++ data_augment.py
@@ -11,19 +11,19 @@ ORIG_INPUT_DATASET = "New Folder/"
 BASE_PATH = ORIG_INPUT_DATASET
 TRAIN_PATH = BASE_PATH
 
-#TRAIN_PATH = os.path.sep.join([BASE_PATH, "train/"+dest])
-#VAL_PATH = os.path.sep.join([BASE_PATH, "valid/"+dest])
-#TEST_PATH = os.path.sep.join([BASE_PATH, "test/"+dest])
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "train/"+dest])
+VAL_PATH = os.path.sep.join([BASE_PATH, "valid/"+dest])
+TEST_PATH = os.path.sep.join([BASE_PATH, "test/"+dest])
 
 TRAIN_SPLIT = 0.8
 VAL_SPLIT = 0.1
 
-#filename = [TRAIN_PATH, VAL_PATH, TEST_PATH]
+filename = [TRAIN_PATH, VAL_PATH, TEST_PATH]
 
-'''for i in filename:
+for i in filename:
 	if not os.path.exists(i):
 		print("\n[INFO] creating '{}' directory".format(i))
-		os.makedirs(i)'''
+		os.makedirs(i)
 
 imagePaths = list(paths.list_images(ORIG_INPUT_DATASET))
 random.seed(42)
@@ -31,12 +31,12 @@ random.shuffle(imagePaths)
 
 	
     
-'''train_images = imagePaths[:int(len(imagePaths)*TRAIN_SPLIT)]
+train_images = imagePaths[:int(len(imagePaths)*TRAIN_SPLIT)]
 val_images = imagePaths[int(len(imagePaths)*TRAIN_SPLIT):int(len(imagePaths)*(TRAIN_SPLIT+VAL_SPLIT))]
-test_images = imagePaths[int(len(imagePaths)*(TRAIN_SPLIT+VAL_SPLIT)):int(len(imagePaths))]'''
+test_images = imagePaths[int(len(imagePaths)*(TRAIN_SPLIT+VAL_SPLIT)):int(len(imagePaths))
 
-'''for i in range(len(train_images)):
-    shutil.copy(train_images[i],TRAIN_PATH)'''
+for i in range(len(train_images)):
+    shutil.copy(train_images[i],TRAIN_PATH)
 
 p = Augmentor.Pipeline(TRAIN_PATH)
 p.invert(0.66)
@@ -50,8 +50,8 @@ p.rotate_without_crop(0.5, 20, 20, expand=False, fillcolor=None)
 p.crop_random(0.5, 0.75, randomise_percentage_area=False)
 p.sample(760)
     
-'''for i in range(len(val_images)):
+for i in range(len(val_images)):
     shutil.copy(val_images[i],VAL_PATH)
 
 for i in range(len(test_images)):
-    shutil.copy(test_images[i],TEST_PATH)'''
+    shutil.copy(test_images[i],TEST_PATH)
